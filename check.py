12ff34ab6ddf4d67ed0c5413c9566e13adfef103
tuanphan09 <tuanptit09@gmail.com>
2019-03-27 21:33:43 +0700

    rm

--- check.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import os
-import pickle
-import cv2
-
-# for file in os.listdir("./tmp"):
-#     img = cv2.imread("./tmp/" + file, 0)
-#     cv2.imwrite("./tmp/gray_" + file, img)
-
